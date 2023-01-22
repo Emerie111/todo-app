14d33ef9df485ae8eaa88f5b9f63c09a6bd8f1c1
Emerie111 <emeriemadu00@gmail.com>
2023-01-22 10:52:37 +0100

    fixed bugs and added update tasks

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Get references to the form, input, and task list
-const form = document.querySelector("form");
+const form = document.querySelector(".form");
 const taskInput = document.querySelector("#task-input");
 const taskList = document.querySelector("#task-list");
 
@@ -19,3 +19,38 @@ function addTask(event) {
   deleteButton.innerText = "Delete";
   deleteButton.addEventListener("click", deleteTask);
   newTask.appendChild(deleteButton);
+
+  // Create an update button and append it to the new task
+  const updateButton = document.createElement("button");
+  updateButton.innerText = "Update";
+  updateButton.addEventListener("click", updateTask);
+  newTask.appendChild(updateButton);
+  
+  // Append the new task to the task list
+  taskList.appendChild(newTask);
+
+  // Clear the task input
+  taskInput.value = "";
+}
+
+// Delete a task from the list
+function deleteTask(event) {
+  // Get the parent list item of the clicked button
+  const taskItem = event.target.parentElement;
+
+  // Remove the task from the list
+  taskList.removeChild(taskItem);
+}
+
+// Update a task in the list
+function updateTask(event) {
+  // Get the parent list item of the clicked button
+  const taskItem = event.target.parentElement;
+  const taskText = taskItem.innerText;
+
+  // Show a prompt to enter the updated task
+  const updatedTask = prompt("Enter the updated task", taskText);
+
+  // Update the task text
+  taskItem.innerText = updatedTask;
+}
